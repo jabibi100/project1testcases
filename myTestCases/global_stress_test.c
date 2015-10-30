6f71faac4b3ffe813e6b00b61f9ad43dc1c2c888
Mohammad-Mohsen Minaei-Bidgoli <mminaeib@ta12.cs.purdue.edu>
2015-10-30 16:11:27 -0400

    updated global_stress_test and if

--- myTestCases/global_stress_test.c
+++ myTestCases/global_stress_test.c
@@ -3,36 +3,36 @@
 *
 */
 
-int g0;
-int g1;
-int g2;
-int g3;
-int g4;
-int g5;
-int g6;
-int g7;
+int g0; //1
+int g1;//2
+int g2;//3
+int g3;//4
+int g4;//5
+int g5;//6
+int g6;//7
+int g7;//8
 
 
 int foo1(int y)
 {
-	int x1;
-	int	x2;
+	int x1;//9
+	int	x2;//10
 
-	g0 = y;
-	g4 = 22;
+	g0 = y;//11
+	g4 = 22;//12
 
-	y = x1;
-	switch( x2 )
+	y = x1;//13
+	switch( x2 )//14
 	{
 		case 1: 
-			x2 = g1; 
-			break;
+			x2 = g1;//15 
+			break;//16
 		case 2: 
-			x2 = g2; 
-			break;
+			x2 = g2; //17
+			break;//18
 		case 3: 
-			x2 = g3; 
-			break;
+			x2 = g3; //19
+			break;//20
 	}
 }
 
@@ -40,14 +40,14 @@ int foo1(int y)
 
 void foo2()
 {
-	int y1, y2;
-	int z1 = y1;
+	int y1, y2;//21 22
+	int z1 = y1;//23
 
-	g5 = foo1(g4);
+	g5 = foo1(g4);//24
 
-	if( y2 > 12 )
+	if( y2 > 12 )//25
 	{
-		printf( "hey, we should print y2 since it is undefined!\n" );
+		printf( "hey, we should print y2 since it is undefined!\n" );//26
 	}
 }
 
@@ -55,59 +55,59 @@ void foo2()
 
 void foo3()
 {
-	int z1;
-	int z2;
+	int z1;//27
+	int z2;//28
 
 	
-	while( z1 < 20 )
+	while( z1 < 20 )//29
 	{
-		printf( "need to exit somehow\n" );
+		printf( "need to exit somehow\n" );//30
 	}
 	
-	switch(z2){
+	switch(z2){//31
 		case 1: 
-			g3 = 110;
-			break;
+			g3 = 110;//32
+			break;//33
 		default: 
-			g3 = 120;
+			g3 = 120;//34
 	}
 }
 
 
 void foo4(int x)
 {
-	int t1, t2;
+	int t1, t2;//35 36
 
-	x = t1;
-	g6 = 12;
+	x = t1;//37
+	g6 = 12;//38
 }
 
 
 void main()
 {
-	int r1, r2;
-	int r3 = 22;
+	int r1, r2;//39 40
+	int r3 = 22; //41
 
-	if( r3 > 22 )
-		g2 = 12;
+	if( r3 > 22 )//42
+		g2 = 12;//43
 
-	if( r1 > 20 )   
-		foo3();
+	if( r1 > 20 )  //44 
+		foo3();//45
 	else 
-		foo3();
+		foo3();//46
 
-	switch(r2) {
+	switch(r2) {//47
 		case 1: 
 		default: 
-			foo2();
+			foo2();//48
 	}
 
-	while(r2>100) foo4(g3);
+	while(r2>100) foo4(g3); // 49  50
 
-	r1 = g0;
-	r2 = g6;
-	g1 = 12;
+	r1 = g0;//51
+	r2 = g6;//52
+	g1 = 12;//53
 
-	printf( "%d %d\n", g5, g6);
-	printf( "g7 is not DEFined so need to print!%d\n", g7 );
+	printf( "%d %d\n", g5, g6); //54
+	printf( "g7 is not DEFined so need to print!%d\n", g7 );//55
 }
--- myTestCases/if.c
+++ myTestCases/if.c
@@ -7,129 +7,129 @@ foo5:y,z
  */
 void foo()
 {
-    int x = 2;
-    int y;
-
-    if( x > 1 )
+    int x = 2; //1
+    int y; //2
+ 
+    if( x > 1 ) //3
     {
-        y = 2;
+        y = 2;//4
     }
     else
     {
-        y = 1;
+        y = 1;//5
     }
 
 
    // "y" should be OK
-   printf( "%d\n", y );
+   printf( "%d\n", y ); //6
 }
 
 void foo2()
 {
-    int x = 1;
-    int y;
+    int x = 1;//7
+    int y;//8
 
-    if( x > 1 )
+    if( x > 1 )//9
     {
-        y = 2;
+        y = 2;//10
     }
-    else if( x < 1 )
+    else if( x < 1 )//11
     {
-        y = 3;
+        y = 3;//12
     }
 
     // "y" should be printed since there is no ELSE:
-    printf( "%d\n", y );
+    printf( "%d\n", y ); //13
 }
 
 void foo3()
 {
-    float y;
-    float z;
-    float w = 1;
+    float y;//14
+    float z;//15
+    float w = 1;//16
 
     // "y" should be printed
-    if( y > 2)
+    if( y > 2)//17
     {
         // "w' is OK
-        printf( "%f\n", w );
-        z = 2.0;
+        printf( "%f\n", w );//18
+        z = 2.0;//19
     }
 
     // "z" should be printed:
-    printf( "%f\n", z );
+    printf( "%f\n", z );//20
 }
 
 void foo4()
 {
-    int y = 1;
-    char z;
+    int y = 1;//21
+    char z;//22
 
-    if( y > 0 )
-        z = 'a';
-    else if( y == 0 )
-        z = 'b';
+    if( y > 0 )//23
+        z = 'a';//24
+    else if( y == 0 )//25
+        z = 'b';//26
     else
-        z = 'c';
+        z = 'c';//27
 
     // "z" should NOT be printed:
-    printf( "%d\n", z );
+    printf( "%d\n", z );//28
 }
 
 
 void foo5()
 {
-    int x = 2;
-    int y;
-    int z;
+    int x = 2;//29
+    int y;//30
+    int z;//31
 
-    if( x > 1 )
-        y = 2;
+    if( x > 1 )//32
+        y = 2;//33
     else
     {
         // "z" should be printed
-        if( z < 2 )
+        if( z < 2 )//34
         {
-            y = 3;
+            y = 3;//35
         }
     }
 
     // "y" should be printed:
-    printf( "%d\n", y );
+    printf( "%d\n", y );//36
 }
 
 
 void foo6()
 {
-    int x = 2;
-    int y;
+    int x = 2;//37
+    int y;//38
 
-    if( x < 1 )
+    if( x < 1 )//39
     {
-        y = 100;
+        y = 100;//40
     }
     else
     {
-        if( x < 100 )
-            y = 101;
+        if( x < 100 )//41
+            y = 101;//42
         else
-            y = 102;
+            y = 102;//43
     }
         
     // "y" should be OK
-    printf( "%d\n", y );
+    printf( "%d\n", y );//44
 }
 
 
 int main(){
 
-foo();
-foo1();
-foo2();
-foo3();
-foo4();
-foo5();
-foo6();
+foo();//45
+foo1();//46
+foo2();//47
+foo3();//48
+foo4();//49
+foo5();//50
+foo6();//51
 
 }
 
