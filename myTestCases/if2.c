5946e428f3e97d17102c7c91bf6505d6316bb706
Mohammad-Mohsen Minaei-Bidgoli <mminaeib@ta12.cs.purdue.edu>
2015-10-30 16:17:03 -0400

    updated if2

--- myTestCases/if2.c
+++ myTestCases/if2.c
@@ -5,20 +5,20 @@ foo:s,w,y,x
  */
 void main()
 {
-    int z = 12;
-    int x;
-    int y;
-    int w;
-    int r = 0, s;
+    int z = 12;//1
+    int x;//2
+    int y;//3
+    int w;//4
+    int r = 0, s;//5 6
 
     // "x" should be printed
-    printf( "%d\n", x == 1 ? 12 : 13 );
+    printf( "%d\n", x == 1 ? 12 : 13 );//7
 
     // "y" should be printed:
-    printf( "%d\n", z == 12 ? y : 13 );
+    printf( "%d\n", z == 12 ? y : 13 );//8
 
     // "w" and "s" should be printed:
-    printf( "%d\n", z == 12 ? (w * 12 + 43) : (r + s) );
+    printf( "%d\n", z == 12 ? (w * 12 + 43) : (r + s) );//9
 }
 
 
