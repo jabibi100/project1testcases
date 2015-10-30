19fb628fe45756ecf3120e1a7d6f76533758fe5a
Mohammad-Mohsen Minaei-Bidgoli <mminaeib@ta12.cs.purdue.edu>
2015-10-30 16:41:00 -0400

    updated while

--- myTestCases/while.c
+++ myTestCases/while.c
@@ -1,19 +1,37 @@
-/*
-foo:x,y
-foo2:z,y
-foo3:y,x,z
- */
+int xx;//30
+int yy;//31
+char cc = 's';//32
+double dd;//33
+float ff =2.0;//34
 void foo()
 {
-    int x;
+    int x; //1
 
-    while ( x < 2 )
+    while ( x < 2 )//2
     {
-        int y;
-        x = 2;
+		while(x!=2){//3
+			if(x==34){//4
+				x = 2;//5
+			}
+			if(x==-1){//6
+				while(x!= -1){//7
+					x++;//8
+					int a = 1;//9	
+					while(a> -2){//10
+						++a;//11
+					}
+					--a;//12
+				}
+			}else{
+				xx++;//13
+				x--;//14
+			}
+		}
+        int y;//15
+        x = 2;//16
 
         // "y" should be printed:
-        printf( "%d\n", y );
+        printf( "%d\n", y );//17
     }
 
     // "x" should be printed:
@@ -22,57 +40,33 @@ void foo()
 
 void foo2()
 {
-    int x = 2;
-    int y;
-    int z;
+    int x = 2;//18
+    int y;//19
+    int z;//20
 
-    while ( x < 2 )
+    while ( x < 2 )//21
     {
-        printf( "%d\n", x );
-        //
-        // "y" should be reported:
-        while( y < 3 )
+        while( y < 3 )//22
         {
-            z = 1;
-            printf( "%d\n", y );
+			switch(x){//23
+				case 1:
+				case 2:
+					if(y>1){//24
+						z = 1;//25
+					}
+					break;//26
+			}
+			continue;//27
         }
         
         // "z" should be reported:
-        printf( "%d\n", z );
+        printf( "%d\n", z );//28
     }
 }
 
-
-void foo3()
-{
-    int x;
-    int y;
-
-    // "x" should be reported
-    while(x > 2)
-    {
-        int z;
-
-        y = 2;
-
-        // "z" should be reported
-        printf( "%d\n", z );
-    }
-
-    // "y" should be reported..
-    printf( "%d\n", y );
-
-#if 0
-    while( x > 2 )
-        while( y > 1 )
-            printf( "hello\n" );
-#endif
-}
 
 int main(){
-	foo();
-	foo2();
-	foo3();
+	foo();//29
 }
 
 
