0f9624495469199aa8a05dfbf7ecb429ecaf6315
Javid Habibi <jabibi100@gmail.com>
2015-10-30 02:06:36 -0400

    updated float and global_stress_test

--- myTestCases/float.c
+++ myTestCases/float.c
@@ -1,31 +1,22 @@
-/*
-watch out for the return of float - I had to handle FLOAT_EXPR
-
-output.txt should have:
-foo:x
-foo3:y,x
- */
 int foo()
 {
-    int x;
+    float x;
 
-    // "x" is uninitialized:
     return x;
 }
 
 void foo2()
 {
-    int y;
+    float y;
 
     return;
 }
 
 float foo3()
 {
-    int x;
-    int y;
+    float x;
+    float y;
 
-    // x and y are uninitialized:
     return x+y;
 }
 
