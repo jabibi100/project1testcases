c5cd33658c4b2a37a87563dcde98219f71f3a5c0
Mohammad-Mohsen Minaei-Bidgoli <mminaeib@ta12.cs.purdue.edu>
2015-10-30 16:23:25 -0400

    updated if2

--- myTestCases/switch.c
+++ myTestCases/switch.c
@@ -1,3 +1,4 @@
+
 /*
 
 output.txt should have:
@@ -10,86 +11,86 @@ foo5:a
 */
 void foo()
 {
-    int x ;
-    int y;
+    int x ; //1
+    int y;//2
 
     // "y" should be printed
-    switch( y )
+    switch( y )//3
     {
         case 1:
         {
-            x = 2;
-            printf( "%d\n", x );
-            break;
+            x = 2;//4
+            printf( "%d\n", x );//5
+            break;//6
         }
 
         case 2:
-            x = 3;
-            printf( "%d\n", x );
-            break;
+            x = 3;//7
+            printf( "%d\n", x );//8
+            break;//9
 
         default:
-            x = 4;
-            printf( "%d\n", x );
-            break;
+            x = 4;//10
+            printf( "%d\n", x );//11
+            break;//12
     }
 
     // "x" should NOT be printed since it is defined in ALL blocks above:
-    printf( "%d\n", x );
+    printf( "%d\n", x );//13
 }
 
 void foo2()
 {
-    int x;
-    int y = 2;
+    int x;//14
+    int y = 2;//15
 
     // y is OK here:
-    switch(y)
+    switch(y)//16
     {
         // fall through example:
         case 1:
         case 2:
         {
-            int z;
+            int z;//17
 
             // should print "z"
-            printf( "%d\n", z );
-            break;
+            printf( "%d\n", z );//18
+            break;//19
         }
 
         case 3:
         default:
             // should print "x"
-            printf( "%d\n", x );
+            printf( "%d\n", x );//20
     }
 }
 
 void foo3()
 {
-    int y;
-    int x;
+    int y;//21
+    int x;//22
 
-    switch(y)
+    switch(y)//23
     {
         case 0:
-            y = 1;
+            y = 1;//24
             // y is OK here:
-            printf( "%d\n", y );
+            printf( "%d\n", y );//25
 
             // fall through
         case 1:
             // should print "y" (not defined in the Case 1 block!)
-            printf( "%d\n", y ); break;
+            printf( "%d\n", y ); break;//26 27
 
         case 2:
-            x = 3; break;
+            x = 3; break;//28 29
 
         default:
-            x = 4; break;
+            x = 4; break;//30 31
     }
 
     // should print "x" since it is not initialized in the first block:
-    printf( "%d\n", x );
+    printf( "%d\n", x );//32
 }
 
 
@@ -97,47 +98,47 @@ void foo3()
 
 void foo4()
 {
-    int y = 1;
-    int x;
+    int y = 1;//33
+    int x;//34
 
     // "y" is OK
-    switch(y*2)
+    switch(y*2)//35
     {
         case 1:
         case 2:
         default:
-            x = 1;
-            break;
+            x = 1;//36
+            break;//37
     }
 
     // should NOT print "x"
-    printf( "x is %d\n", x );
+    printf( "x is %d\n", x );//38
 }
 
 void foo5()
 {
-    float a;
-    float c;
-    int b = 2;
+    float a;//39
+    float c;//40
+    int b = 2;//41
 
-    switch( b )
+    switch( b )//42
     {
         case 1:
-            a = 1.0; break;
+            a = 1.0; break;//43 44
         case 2:
-            a = 2.0; break;
+            a = 2.0; break;//45 46
     }
 
     // "a" is uninitialized since there is not DEFAULT block above.
-    c = a;
+    c = a;//47
 }
 
 int main(){
 
-	void foo();
-	void foo2();
-	void foo3();
-	void foo4();
-	void foo5();
+	void foo();//48
+	void foo2();//49
+	void foo3();//50
+	void foo4();//51
+	void foo5();//52
 }
 
